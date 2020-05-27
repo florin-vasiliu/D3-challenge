5586ed643b8465a92ee50eaff98786221454bf30
Florin Vasiliu <favasiliu@gmail.com>
2020-05-26 21:07:43 -0500

    added datasource

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 // @TODO: YOUR CODE HERE!
+// Retrieve data from the CSV file and execute everything below
+d3.csv("assets/data/data.csv").then(function(data, err) {
+    if (err) throw err;
+  
+    // parse data
+    data.forEach(function(dataRow) {
+        dataRow.healthcare = +dataRow.healthcare;
+    });
+    console.log(data)
+}).catch(function(error) {
+    console.log(error);
+  });
